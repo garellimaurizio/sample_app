1cb733f008d5516bde8adf368735e0245059e8fb
Maurizio Garelli <garellimaurizio@outlook.com>
2015-06-06 16:33:36 +0200

    Fixed heroku host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,7 +80,7 @@ Rails.application.configure do
   #Enable email send
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = '<your heroku app>.herokuapp.com'
+  host = 'sample-app-garelli.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
